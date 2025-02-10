1ebf862e5abecd8b2a4431025a1a412fac781a48
Vincent <vincent.brunelle.gm@gmail.com>
2025-02-09 19:44:55 -0500

    ADD : Field validation and error messages in login

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
+const {
+  body,
+  ValidationChain,
+  validationResult,
+} = require("express-validator");
 
 const express = require("express");
 const app = express();
@@ -16,31 +21,75 @@ app.get("^/$|/index(.html)?", (req, res) => {
 app
   .route("/login(.html)?")
   .get((req, res) => {
-    res.render("pages/login");
+    return res.render("pages/login", {
+      errors: [],
+      formData: {},
+    });
   })
-  .post((req, res) => {
-    let first_name = req.body.firstName;
-    let last_name = req.body.lastName;
-    let email = req.body.email;
-    let birth_date = new Date(req.body.birthDate);
-    let password = req.body.password;
-    let password_confirmation = req.body.passwordConfirmation;
-    console.log(
-      JSON.stringify(
-        {
-          first_name,
-          last_name,
-          email,
-          birth_date,
-          password,
-          password_confirmation,
-        },
-        null,
-        2
-      )
-    );
-    res.render("pages/login");
-  });
+  .post(
+    [
+      body("firstName")
+        .notEmpty()
+        .withMessage("First name is required")
+        .matches(/^[a-zA-ZÀ-ÿ\s'\-~]+$/)
+        .withMessage("Invalid characters"),
+      body("lastName")
+        .notEmpty()
+        .withMessage("Last name is required")
+        .matches(/^[a-zA-ZÀ-ÿ\s'\-~]+$/)
+        .withMessage("Invalid characters"),
+      body("email").isEmail().withMessage("Email adress is not valid"),
+      body("birthDate").isDate().withMessage("Date is not valid"),
+      body("password")
+        .matches(/[a-z]/)
+        .withMessage("Password must contain at least one lower case letter")
+        .matches(/[A-Z]/)
+        .withMessage("Password must contain at least one capital letter")
+        .matches(/[!@#$%^&*]/)
+        .withMessage(
+          "Password must contain at least one of these symbols : !@#$%^&*"
+        ),
+      body("passwordConfirmation")
+        .equals(body("password"))
+        .withMessage("Both password must be identical"),
+    ],
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors);
+        console.log(req.body);
+        return res.render("pages/login", {
+          errors: errors.array(),
+          formData: req.body,
+        });
+      }
+      let first_name = req.body.firstName;
+      let last_name = req.body.lastName;
+      let email = req.body.email;
+      let birth_date = new Date(req.body.birthDate);
+      let password = req.body.password;
+      let password_confirmation = req.body.passwordConfirmation;
+      console.log(
+        JSON.stringify(
+          {
+            first_name,
+            last_name,
+            email,
+            birth_date,
+            password,
+            password_confirmation,
+          },
+          null,
+          2
+        )
+      );
+      res.render("pages/login");
+    }
+  );
+
+app.get("/gallery(.html)?", (req, res) => {
+  res.render("pages/gallery");
+});
 
 app.listen(5500, (req, res) => {
   console.log(`Server running!`);
